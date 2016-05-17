1f5c91c05516324841f321a94cb2e894f54fc94f
Zhili Hua <zhilihua@Zhili-MBP.fios-router.home>
2016-05-17 00:05:14 -0400

    001 final

--- 001_TwoSum.js
+++ 001_TwoSum.js
@@ -1,3 +1,22 @@
 /**
- * Created by zhilihua on 5/16/16.
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
  */
+var twoSum = function(nums, target) {
+    var ret = [];
+    var map = {};
+    for(var i = 0; i < nums.length; i++) {
+        var curr =  nums[i];
+        if(map.hasOwnProperty(curr.toString())) {
+            ret.push(map[curr.toString()]);
+            ret.push(i);
+            break;
+        }
+        else {
+            curr =  target - nums[i];
+            map[curr.toString()] = i;
+        }
+    }
+    return ret;
+};
