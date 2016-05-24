255aafbf323c891134c546bdaf7f1f6f5c3f8e8f
Zhili Hua <zhilihua@Zhili-MBP.fios-router.home>
2016-05-23 20:26:43 -0400

    024

---
+++ 024_SwapNodesInPairs.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+    var pre_head = new ListNode(0);
+    pre_head.next = head;
+    var curr_head = pre_head;
+    while(curr_head.next != null && curr_head.next.next != null) {
+        var end_node = curr_head.next.next.next;
+        var next_node = curr_head.next;
+        var next_next_node = curr_head.next.next;
+
+        curr_head.next = next_next_node;
+        next_next_node.next = next_node;
+        next_node.next = end_node;
+        curr_head = next_node
+
+    }
+    return pre_head.next;
+};
